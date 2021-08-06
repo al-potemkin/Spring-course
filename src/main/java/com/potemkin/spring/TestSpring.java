bd713e47b471272e7a57c5788bc9c67598c6a450
Potemkin <potemkin.kernel@gmail.com>
2021-08-06 16:34:25 +0300

    create beans from xml document

---
+++ src/main/java/com/potemkin/spring/TestSpring.java
@@ -0,0 +1,15 @@
+package com.potemkin.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+
+        //необходимо закрывать контекст
+        context.close();
+    }
+}
